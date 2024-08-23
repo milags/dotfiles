9b517a6c6098189a4a3da2f841e50c28f6fd1f59
Mila Goldsmith <milags@github.com>
2024-08-23 11:06:59 -0400

    Add completion message

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 mkdir -p "$HOME/.zsh"
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
-
+echo done
